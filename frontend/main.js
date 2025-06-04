fd8f8d2b32baf55f1ee8b51a674162f9ead0c322
El Wiysman <elwiysman@gmail.com>
2025-06-04 19:42:37 +0700

    Update main.js untuk perubahan terbaru

--- frontend/main.js
+++ frontend/main.js
@@ -11,11 +11,11 @@ import {
   initScrollToTop,
 } from "./scripts/ui.js";
 
-//export const API_BASE_URL = "http://localhost:5000";
-export const API_BASE_URL =
-  window.location.hostname === "localhost"
-    ? "http://localhost:5000"
-    : "https://de34-103-18-34-184.ngrok-free.app";
+export const API_BASE_URL = "http://localhost:5000";
+//export const API_BASE_URL =
+//  window.location.hostname === "localhost"
+//    ? "http://localhost:5000"
+//    : "https://6810-103-18-34-184.ngrok-free.app";
 export let currentOperation = "aritmatika";
 export let isCalculating = false;
 
